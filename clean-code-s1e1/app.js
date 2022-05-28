ab86d27cf93923ab41dff06a071d1a9db840d48a
kosta4310 <realanka77@gmail.com>
2022-05-28 16:40:01 +0300

    refactor: adjust section of class 'add-item' according to rule 3.4 in html-and-css.md and 2.1 in html-and-css-extended.md

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -41,9 +41,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit button";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
