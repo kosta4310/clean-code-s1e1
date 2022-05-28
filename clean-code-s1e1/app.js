89da67a50d91a187108105f3b35470a7dbfd608b
kosta4310 <realanka77@gmail.com>
2022-05-28 22:40:04 +0300

    refactor: still adjust HTML and CSS formatting according to rule 1.3 in html-and-css.md

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -33,7 +33,7 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task label';
+    label.className="task label";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
@@ -45,7 +45,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="edit button";
 
     deleteButton.className="delete button";
-    deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.src="./remove.svg";
     deleteButtonImg.className= "image-delete";
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,7 +84,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
